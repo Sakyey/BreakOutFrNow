69d15cea9ca8bc2e100b65ed6d951c3ec8930e52
Sakyey <gustaf.m.dahlqvist@gmail.com>
2023-09-23 20:22:16 +0200

    Add project files.

---
+++ Breakout134/Class1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakout134
+{
+    public class Boll
+    {
+        Texture2D bolltex;
+        Vector2 bollPos;
+        Vector2 bollDir;
+        Rectangle bollRec;
+        public bool crash;
+        bool changeDir;
+        int maximumX = 800;
+        int maximumY = -600;
+        int minimumX = 0;
+        int minimumY = 0;
+        public Boll(Texture2D bolltex, Vector2 bollPos)
+        {
+            this.bolltex = bolltex;
+            this.bollPos = bollPos;
+            bollRec = new Rectangle((int)bollPos.X, (int)bollPos.Y, 50, 50);
+            crash = false;
+            bollDir = new Vector2(7, -4);
+            changeDir = false;
+        }
+        public void Update()
+        {
+            bollPos.X = bollPos.X + bollDir.X;
+            bollPos.Y = bollPos.Y + bollDir.Y;
+        }
+        public void CheckCrash(Rectangle block)
+        {
+            if (bollRec.Intersects(block))
+            {
+                crash = true;
+                if (bollPos.X < maximumX && bollPos.X> minimumX)
+                {
+                    changeDir = false;
+                }
+                else
+                {
+                    bollDir.X = bollDir.X * -1;
+                    changeDir = true;
+                }
+                if (bollPos.Y > maximumY && bollPos.Y< minimumY)
+                {
+                    changeDir = false;
+                }
+                else
+                {
+                    bollDir.Y = bollDir.Y * -1; 
+                    changeDir = true;
+                }
+
+            }
+        }
+    }
+}
---
+++ Breakout134/Game1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Breakout134
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D bollen;
+        Texture2D padle;
+        Texture2D texture;
+        Texture2D block;
+        Vector2 bollPos;
+        Vector2 bollDir;
+
+        Texture2D[] blockArray;
+
+        MouseState mouseState;
+        Vector2 mousePos;
+        Boll boll1;
+        Boll boll;
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            bollen = Content.Load<Texture2D>("ball_breakout");
+            bollPos = new Vector2(50, 50);
+            block = Content.Load<Texture2D>("block_breakout");
+
+            padle = Content.Load<Texture2D>("padle");
+            bollDir = new Vector2(5, -5);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            //bollPos.X = bollPos.X + bollDir.X;
+            //bollPos.Y = bollPos.Y + bollDir.Y;
+
+            boll1.Update();
+            //boll1.CheckCrash();
+            //boll.CheckCrash(padle);
+            // TODO: Add your update logic here
+            mouseState =Mouse.GetState();
+            mousePos.X = mouseState.X - 73;
+            mousePos.Y = 350;
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            
+            _spriteBatch.Draw(bollen, bollPos, Color.Yellow);
+            _spriteBatch.Draw(padle, mousePos, Color.Red);
+            
+            _spriteBatch.End();
+
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+        
+    }
+}
